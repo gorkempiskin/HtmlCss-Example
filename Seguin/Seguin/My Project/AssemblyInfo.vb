1d9aeac4d87b82ff504d33fc36956f9a3e9f16ef
gorkem <g.piskin@outlook.com>
2020-01-20 19:09:27 +0300

    HtmlCss Example

---
+++ Seguin/Seguin/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir derlemeyle ilgili Genel Bilgiler aşağıdaki öznitelik kümesi kullanılarak 
+' denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+' öznitelik değerlerini değiştirin.
+
+' Derleme özniteliklerinin değerlerini incele
+<Assembly: AssemblyTitle("Seguin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Seguin")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Aşağıdaki GUID, bu proje COM'a gösterildiğinde typelib Kimliği içindir.
+<Assembly: Guid("634edd04-581b-46de-8b5b-865f9d667482")>
+
+' Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Tüm değerleri belirtebilirsiniz veya Yapı ve Düzeltme Numaralarına aşağıda 
+' gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
